52399db60f9f245b77b87b6a076601f0fb69ce2f
Demo User <debian@beaglebone.localdomain>
2019-04-03 03:12:58 +0000

    usando escala de microsegundo

--- trabalho2/main.cpp
+++ trabalho2/main.cpp
@@ -4,8 +4,9 @@
 #include <new>
 #include <queue>
 #include <cstdlib>
-#include <ctime> 
+//#include <ctime> 
 #include <signal.h>
+#include <sys/time.h>
 #include "../../BlackGPIO/BlackGPIO.h"
 #include "../../ADC/Adc.h"
 #include "../../PWM/PWM.h"
@@ -19,39 +20,12 @@ enum leds {
 	RIGHT
 };
 
-//ganhando: piscar tudo	
-void ganhador(Display &disp, BlackGPIO *pinos_leds, BlackGPIO)
+float difftime(struct timeval &fim, struct timeval &comeco)
 {
-	
-	while(true)
-	{
-		disp.show(9);
-		pinos_leds[LEFT].setValue(high);		
-		pinos_leds[CENTER].setValue(high);		
-		pinos_leds[RIGHT].setValue(high);
-		
-		usleep(500000);
-		
-		disp.clear();
-		pinos_leds[LEFT].setValue(low);		
-		pinos_leds[CENTER].setValue(low);		
-		pinos_leds[RIGHT].setValue(low);
-		
-		usleep(500000);
-	}
-	
+	float diferenca = fim.tv_sec - comeco.tv_sec; 
+    diferenca = diferenca + (fim.tv_usec - comeco.tv_usec) * 1e-6; 
+    return diferenca;
 }
-
-void perdedor(Display &disp, const BlackGPIO &reset)
-{
-	disp.show(0);
-		
-	usleep(500000);
-	
-	disp.clear();
-	
-	usleep(500000);
-}	  
 
 int main()
 {
@@ -60,61 +34,58 @@ int main()
 	PWM indicador_tempo(P9_22);
 	indicador_tempo.setState(statePwm::stop);
 
+	//GPIO dos botões
 	BlackGPIO botoes[4] = {
 							BlackGPIO(GPIO_47, input), 
 	                        BlackGPIO(GPIO_27, input), 
 	                        BlackGPIO(GPIO_7, input), 
 	                        BlackGPIO(GPIO_20, input)
 	                      };
-	                        
+	
+	//GPIO dos leds                        
 	BlackGPIO pinos_leds[3] = {
 						BlackGPIO(GPIO_30, output),
 						BlackGPIO(GPIO_31, output),
 						BlackGPIO(GPIO_48, output)
 						};
 	                        
-    //Insere pinos de 1-8 do display
+    //Indica os pinos do display
 	gpioName portas_display[8] = {GPIO_22, GPIO_15, GPIO_14, GPIO_4, GPIO_5, GPIO_49, GPIO_3, GPIO_51};    
-    Display display(portas_display, 8);
+    Display display(portas_display, 8);  
     
-	float dificuldade = potenciometro.getPercentValue();
+    int pontos = 4;
+    int n_sequencia = 6;
     
-    
-    int fase = 4;
-    int n_sequencia = 2;
-    
-    while(fase < 10 && fase > 0)
+    //Continuar enquanto o jogador não tiver perdido nem ganhado
+    while(pontos < 10 && pontos > 0)
     {
-		//Deve ser preenchido aleatóriamente
-		int * sequencia_leds;
-	  	
-	  	// Nível (Fase) que o jogador se encontra
-	  	// Ele começa na fase 5
-	  	
-	  
-	  	// Sequencia do jogador
+		// Squencia dos leds que irão acender
+		int * sequencia_leds = new int[n_sequencia];
+		
+	  	// Sequencia inserida pelo jogador
 	  	queue<int> jogadas;
 	  	
-	  	time_t comeco_do_turno;
-	  	time_t tempo_atual;
+        // Váriáveis usadas para contabilizar o tempo
+	  	//time_t comeco_do_turno;
+	  	//time_t tempo_atual;
+	  	struct timeval comeco_do_turno, tempo_atual;
 	  
-		// 1. Turno do computador
-	  
-		// 1.1 Gerar a sequencia aleatória de leds
-		
-		display.show(fase);
-	  	sequencia_leds = new int[n_sequencia];
-	  	
-	  	//espera antes de começar
+		display.show(pontos);
+      
+	  	//Espera 2 segundos antes de começar
 		sleep(2);
+        
+        //Tempo total de resposta que varia de 400 ms até 1.5s por elemento da sequencia
+        float timeout = (0.5 + potenciometro.getPercentValue()/100)*n_sequencia;
+        cout << "O tempo de resposta total será de : " << timeout << " segundos" << endl;
 	  	
+      	//Escolhe randomicamente sequencia de leds
 		for (int i = 0; i < n_sequencia ; i++)
 		{
 			sequencia_leds[i] = rand()%3;
 		}
 
-		// 1.2 Ligar os leds na sequencia
-
+		//Acende os leds na sequencia escolhida acima
 		for (int i = 0; i < n_sequencia ; i++)
 		{
 		     // Acender o led, dar sleep, apaga led
@@ -123,64 +94,57 @@ int main()
 		     pinos_leds[sequencia_leds[i]].setValue(low);
 		     usleep(500000);
 		}
-
-		// 1.3 Apagar os leds
-
-		// 1.4 Bota o contador para contar
-		// Ligar o contador 
-
-
-		// 2.  Turno do humano
-		  
-		
-		
-		//A dificuldade é um número entre 0 e 100. O tempo por elemento
-		//na sequencia varia de 500 ms até 1.5s.
-		//int sequencia = leds_acesos.size();
-		int timeout = (0.5 + 1*dificuldade/100)*n_sequencia;
-		//mutex mtx;
-		//bool timeout = false;
-		
-		//thread th1(buttonHandler, &mtx, botoes, &timeout, jogadas); 
-		//th1.join();  
-	  
-	  	// 2.  Turno do humano
-	  	//espera 3 s entre sequencia e repetição (conforme requisitos)
+			  
+	  	//Espera 3 s entre sequencia e repetição (conforme requisitado)
 	  	sleep(3);
-		cout << "Repita a sequência" << endl;
 		
-		time(&comeco_do_turno);
-		time(&tempo_atual);
+		//time(&comeco_do_turno);
+		//time(&tempo_atual);
+		gettimeofday(&comeco_do_turno, NULL);
+		gettimeofday(&tempo_atual, NULL);
 		
-		int periodo = 0;
+		bool reset = false; 
 		pid_t processo = fork();
 		
-		//processo filho lida com PWM
-		
+		//Processo filho lida com PWM enquanto o pai com os botões		
 		if(processo == 0)
 		{
+            //Modifica frequencia do indicador do led tempo dependendo do tempo restante
 			int periodo = 0;
-		indicador_tempo.setState(statePwm::run);
+			indicador_tempo.setState(statePwm::run);
 			while(difftime(tempo_atual,comeco_do_turno) < timeout)
 			{
-				periodo = (timeout - difftime(tempo_atual, comeco_do_turno))*50000000;
-				indicador_tempo.setPeriod(periodo);    	
-				indicador_tempo.setDutyCycle(periodo/2);
-				time(&tempo_atual);
+				//Multiplica diferença por 2 e trunca para que haja variação de frequência de PWM
+				//a cada 0.5 s
+				periodo = ((int)((timeout - difftime(tempo_atual, comeco_do_turno))*2))*25000000;
+				
+				//Estipula periodo mínimo de PWM
+				if(periodo >= 50000000)
+				{
+					indicador_tempo.setPeriod(periodo);    	
+					indicador_tempo.setDutyCycle(periodo/2);
+				}
+				//time(&tempo_atual);
+				gettimeofday(&tempo_atual, NULL);
 			}			
 			
 			exit(1);
 		}
 		else
-		{	  
-			bool reset = false;  
-			while(difftime(tempo_atual,comeco_do_turno) < timeout && jogadas.size() < n_sequencia)
+		{	 
+			cout << "Repita a sequência exibida" << endl;
+            //Loop enquanto a diferença de tempo for menor que "timeout" e o número de elementos na sequência inserida
+            //for inferior a sequencia gerada randomicamente
+			while(difftime(tempo_atual,comeco_do_turno) < timeout && jogadas.size() < (unsigned) n_sequencia)
 			{		
 				//LED 1
 				if(botoes[LEFT].isHigh())
 				{
+                    //Aguarda o botão ser solto e verifica se o intervalo ainda é menor que o tempo total,
+                    //antes de inserir elemento na fila
 					while(botoes[LEFT].isHigh());
-					time(&tempo_atual);
+					//time(&tempo_atual);
+					gettimeofday(&tempo_atual, NULL);
 					if(difftime(tempo_atual, comeco_do_turno) > timeout) break;
 					cout << "Botão 0 pressionado" << endl;
 					jogadas.push(LEFT);
@@ -188,8 +152,11 @@ int main()
 				//LED 2
 			  	else if (botoes[CENTER].isHigh())
 				{
+                    //Aguarda o botão ser solto e verifica se o intervalo ainda é menor que o tempo total,
+                    //antes de inserir elemento na fila
 					while(botoes[CENTER].isHigh());
-					time(&tempo_atual);
+					//time(&tempo_atual);
+					gettimeofday(&tempo_atual, NULL);
 					if(difftime(tempo_atual, comeco_do_turno) > timeout) break;
 					cout << "Botão 1 pressionado" << endl;
 					jogadas.push(CENTER);
@@ -197,8 +164,11 @@ int main()
 				//LED 3
 			  	else if (botoes[RIGHT].isHigh())
 				{
+                  	//Aguarda o botão ser solto e verifica se o intervalo ainda é menor que o tempo total,
+                    //antes de inserir elemento na fila
 					while(botoes[RIGHT].isHigh());
-					time(&tempo_atual);
+					//time(&tempo_atual);
+					gettimeofday(&tempo_atual, NULL);
 					if(difftime(tempo_atual, comeco_do_turno) > timeout) break;
 					cout << "Botão 2 pressionado" << endl;
 					jogadas.push(RIGHT);
@@ -206,35 +176,32 @@ int main()
 				//RESET
 				else if (botoes[3].isHigh())
 				{
+                    //Aguarda botão ser solto, em seguida mudando a flag "reset" para true e parando o loop
 					while(botoes[3].isHigh());
 					cout << "Reset pressionado" << endl;
 					reset = true;
 					break;
 				}
-				time(&tempo_atual);
+				//time(&tempo_atual);
+				gettimeofday(&tempo_atual, NULL);
 			}
-			kill(processo, SIGKILL);	    
-			if(reset) break;
+          
+            //Finaliza processo filho manualmente se o tempo de inserção tiver sido menor que o tempo total
+			kill(processo, SIGKILL);
 		}
 		
-		indicador_tempo.setDutyCycle(0);
+        //Desliga PWM
+		indicador_tempo.setState(statePwm::stop);	
+      
+      	//Pára programa se o botão reset for pressionado
+		if(reset) break;
 		
-		//ganhando: piscar tudo
-		//perdendo: pisca zero
-		//começar: aparece ponto, termino: some ponto
-		
-		// 2.1 Receber entradas
-
-		// 2.2 Comparar com a do computador
-
-		// 2.3. Ver se o cara acertou ou higou com o jogo e aumentar/diminuir o score
-			// Se ele acertou fase++
-	  		// se ele errou, fase--
+        //Esvazia fila da sequencia inserida pelo jogador conferindo quantos foram inseridos
+        //corretamente
 		int acertos = 0;
 		int i = 0;
 		while(!jogadas.empty())
 		{
-		  	cout << "jogada: " <<  jogadas.front() << endl;
 			if(sequencia_leds[i] == jogadas.front())
 			{
 		    	acertos++;
@@ -243,24 +210,31 @@ int main()
 			i++;
 		}
 	  
+        //Incrementa ou Decrementa os pontos se o jogador tiver acertado toda a sequencia
 	  	if(acertos == n_sequencia)
-		  fase++;
+	  	{
+			pontos++;
+			cout << "Você acertou" << endl;
+		}
 	  	else
-		  fase--;
+	  	{
+			pontos--;
+			cout << "Você errou" << endl;
+		}
 	  
 		
-		// 2.4 resetar a sequencia de leds
+		//deleta sequência de leds
 		delete[] sequencia_leds;
-		
     }
     
-    
+    //Pára PWM no fim do jogo;
 	indicador_tempo.setState(statePwm::stop);
-    // 3.   Ver o estado do jogo 
 
-    // 3.1  Ver se ele perdeu/ganhou
-	if(fase == 0)
+    //Verifica se o jogador ganhou ou perdeu
+	if(pontos == 0)
     {
+		cout << "Você perdeu" << endl;
+    	//Enquanto reset não for pressionado, pisca o valor 0 no display
     	while(!botoes[3].isHigh())
     	{
 			display.show(0);
@@ -276,8 +250,10 @@ int main()
 			usleep(500000);
 		}
     }
-  	else if(fase == 10)
+  	else if(pontos == 10)
     {
+		cout << "Você ganhou" << endl;
+    	//Enquanto reset não for pressionado, pisca leds e o valor 9 no display
     	while(!botoes[3].isHigh())
     	{
 			display.show(9);
@@ -299,10 +275,13 @@ int main()
 			usleep(500000);
 		}
     }
-    // 3.2  Zera o timer e para ele
+    
 
-
+	//Limpa display e apaga leds
     display.clear();
+    pinos_leds[LEFT].setValue(low);		
+	pinos_leds[CENTER].setValue(low);		
+	pinos_leds[RIGHT].setValue(low);
     cout << "fim" << endl;
     return 0;
 }
